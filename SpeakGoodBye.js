423d04641d4064d61b53a393843f37b383ab3040
zhndmmn <107850777+zhndmmn@users.noreply.github.com>
2026-04-10 12:35:01 +0500

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function (window) {
+  var byeSpeaker = {};
+  var speakWord = "Goodbye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
